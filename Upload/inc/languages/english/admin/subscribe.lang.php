a1d4029fdc6409c321addfd8fa7a6a7ef5b7cbe2
Mark Vincent <admin@rantcentralforums.com>
2016-10-30 12:25:59 -0500

    Language adjustments

--- Upload/inc/languages/english/admin/subscribe.lang.php
+++ Upload/inc/languages/english/admin/subscribe.lang.php
@@ -9,7 +9,7 @@
 
 $l['subscribe'] = 'AJAX Subscribe';
 
-$l['subscribe_description1'] = 'instant subscription';
+$l['subscribe_description1'] = 'instantly subscribe/unsubscribe threads and forums ';
 $l['subscribe_description2'] = '';
 
 $l['subscribe_logo'] = 'AJAX Subscribe';
